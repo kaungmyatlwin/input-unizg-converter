15ab9450264ae229ff17ef5b2b986ec88e426f97
Kaung Myat Lwin <kaungmyatlwin@nexlabs.co>
2017-07-29 12:08:20 +0630

    clean up code

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,38 +1,29 @@
 (function(){
-	// 17 = ctrl
-	// 220 = backslash
-
-	console.log('is this running? good');
-
 	var keyMap = {};
-
 	// great read - https://stackoverflow.com/questions/5203407/javascript-multiple-keys-pressed-at-once
 	onkeydown = onkeyup = function(e){
-    keyMap[e.keyCode] = e.type == 'keydown';
+		keyMap[e.keyCode] = e.type == 'keydown';
 		if(keyMap[17] && keyMap[220]) {
-			var range = document.createRange();
 			var activeElement = document.activeElement;
 			var defaultValue = activeElement.value;
 			var selectedText = getSelectionText();
 			var selectedNode = window.getSelection().anchorNode.parentNode;
-
 			if(knayi.fontDetect(selectedText) === 'zawgyi') {
-					activeElement.value = knayi.fontConvert(selectedText, 'unicode');
+				activeElement.value = knayi.fontConvert(selectedText, 'unicode');
+			} else if (knayi.fontDetect(selectedText) === 'zawgyi') {
+				activeElement.value = knayi.fontConvert(selectedText, 'zawgyi');
 			} else {
-					activeElement.value = knayi.fontConvert(selectedText, 'zawgyi');
+				activeElement.value = defaultValue;
 			}
-
 		}
 	}
-
 	function getSelectionText(selectStart, selectEnd) {
-    var text = "";
-    if (window.getSelection) {
-        text = window.getSelection().toString();
-    } else if (document.selection && document.selection.type != "Control") {
-        text = document.selection.createRange().text;
-    }
+		var text = "";
+		if (window.getSelection) {
+			text = window.getSelection().toString();
+		} else if (document.selection && document.selection.type != "Control") {
+			text = document.selection.createRange().text;
+		}
     return text;
 	}
-
 })();
